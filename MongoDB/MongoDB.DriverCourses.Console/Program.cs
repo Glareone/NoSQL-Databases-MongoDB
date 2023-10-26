f18204649eb2471bc9181f7378c9547c80bf8124
Aleksey Kolesnikov <>
2023-10-26 16:05:17 +0300

    console app

---
+++ MongoDB/MongoDB.DriverCourses.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Driver;
+
+var connectionString = "mongodb://localhost:27017";
+var client = new MongoClient(connectionString);
+var dblist = await client.ListDatabases().ToListAsync();
+
+foreach (var dbInstance in dblist)
+{
+    Console.WriteLine($"db instance: {dbInstance}");
+}
